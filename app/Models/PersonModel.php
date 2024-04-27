41000288387356b788391c634b402ea2e04b8361
mandrindra12 <mandrindraantonnio@gmail.com>
2024-04-27 17:56:49 +0300

    allowedFields added

--- app/Models/PersonModel.php
+++ app/Models/PersonModel.php
@@ -6,7 +6,21 @@ use CodeIgniter\Model;
 
 class PersonModel extends Model {
   protected $table = 'personnes';
-  protected $allowedFields = [];
+  protected $primaryKey = 'id_personne';
+  protected $allowedFields = [
+    'id_personne',
+    'nom',
+    'prenoms',
+    'date_naissance',
+    'lieu_naissance',
+    'addresse',
+    'CIN',
+    'mail',
+    'telephone',
+    'genre',
+    'nationalite',
+    'id_statut'
+  ];
   public function exist($n, $p) {
     $c = ['nom' => $n, 'prenoms' => $p];
     $count = $this->where($c)->countAllResults();
