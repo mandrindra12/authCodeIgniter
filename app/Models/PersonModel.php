87e6457dac9f75cef80007d84000e60ea3b017a4
mandrindra12 <mandrindraantonnio@gmail.com>
2024-04-28 16:34:00 +0300

    update maivana

--- app/Models/PersonModel.php
+++ app/Models/PersonModel.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class PersonModel extends Model {
+  protected $db;
+  protected $builder;
   protected $table = 'personnes';
   protected $primaryKey = 'id_personne';
   protected $allowedFields = [
@@ -21,10 +23,17 @@ class PersonModel extends Model {
     'nationalite',
     'id_statut'
   ];
+  public function __construct() {
+    $this->db = db_connect();
+    $this->builder = $this->db->table($this->table); 
+  }
   public function exist($n, $p) {
     $c = ['nom' => $n, 'prenoms' => $p];
     $count = $this->where($c)->countAllResults();
     return $count > 0;
+  }
+  public function getRelativeInfo($nom, $prenoms) {
+    return $this->builder->select('id_personne, id_statut')->where(['nom'=> $nom, 'prenoms'=> $prenoms])->get()->getResultArray();
   }
 }
 
