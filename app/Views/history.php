b58c7e88be1d2228244a07b8fd479bcd0b388510
lalakely <rakotoherilala04@gmail.com>
2024-05-06 21:46:35 +0300

    Ce fichier est pour le view des historiques

---
+++ app/Views/history.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MIT | History</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+        th, td {
+            padding: 10px;
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+        th {
+            background-color: #FF3131; /* Couleur bleue */
+            color: white; /* Texte en blanc */
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <thead> 
+                <th>id</th>
+                <th>action</th>
+                <th>table_name</th>
+                <th>date</th>
+            </thead>
+            <tbody>
+                <?php foreach($history as $tracker) :?>
+                    <tr>
+                        <td>$tracker['id']</td>
+                        <td>$tracker['action']</td>
+                        <td>$tracker['table_name']</td>
+                        <td>$tracker['date']</td>
+                    </tr>
+                <?php endforeach;?>
+            </tbody>
+        </tr>
+    </table>
+</body>
+</html>
