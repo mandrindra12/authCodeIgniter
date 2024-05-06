e69368270e3e97114112cda709358c98b1262842
lalakely <rakotoherilala04@gmail.com>
2024-05-06 20:57:16 +0300

    Change la visibilité de l'accueil

--- app/Views/accueil.php
+++ app/Views/accueil.php
@@ -1,16 +1,49 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+     background-color: #f1f1f1;
+      background-size: cover;
+      background-position: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100vw;
+      height: 100vh;
+    }
+    .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: #fff;
+            border-radius: 20px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+            padding: 40px;
+            position: relative;
+    }
+  </style>
+  <title>Accueil test</title>
+</head>
+<body>
+  
+</body>
+</html>
 <html>
   <body>
-    OK Connecter 
-    <?php 
-    if($data['statut'] == 1){
-      echo "MR" . $data['nom'];
-    } else {
-      echo "Mpianatra" . $data['prenom'];
-    }
-
-    ?>
-    <a href='/deconnexion'> deconnexion </a>
+    <div class="container">
+        OK Connecter 
+        <?php 
+        if($data['statut'] == 1){
+          echo "MR" . $data['nom'];
+        } else {
+          echo "Mpianatra" . $data['prenom'];
+        }
+        ?>
+        <a href='/deconnexion'> deconnexion </a>
+    </div>
   <body>
-
 </html>
