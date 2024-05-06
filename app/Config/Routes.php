9b87ecd65ba1061e968720685452f5c08f321ccf
mandrindra12 <mandrindraantonnio@gmail.com>
2024-05-06 10:04:58 +0300

    route for qrcode login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,6 +11,7 @@ $routes->get('/inscription', 'UserController::inscriptionIndex');
 $routes->get('/connexion', 'UserController::index');
 $routes->get('/accueil', 'UserController::accueil');
 $routes->get('/deconnexion', 'UserController::deconnexion');
+$routes->get('/qrconnect', 'UserController::connexion');
 # post 
 $routes->post('/connexion', 'UserController::connexion');
 $routes->post('/inscription', 'UserController::inscription');
