59fcc9ae2af80540b5ded89ae1cb9c3fc566bb92
mandrindra12 <mandrindraantonnio@gmail.com>
2024-05-16 12:02:31 +0300

    fixed css

--- app/Views/signup.php
+++ app/Views/signup.php
@@ -80,7 +80,7 @@
             padding: 12px;
             cursor: pointer;
             transition: background-color 0.3s ease;
-        }  background-color: #ff0000;
+        }
 
         .button button:hover {
             background-color: #dd1111;
@@ -109,6 +109,7 @@
         .link a:hover {
             color: #de1111;
         }
+
         .popup {
             display: none;
             position: fixed;
@@ -173,6 +174,7 @@
                 opacity: 1;
             }
         }
+
         .loader-container {
             display: none;
             position: fixed;
@@ -181,9 +183,10 @@
             top: 0;
             width: 100%;
             height: 100%;
-            overflow: auto; 
+            overflow: auto;
             background-color: rgba(0, 0, 0, 0.4);
         }
+
         /* OLD LOADER */
         /* .loader {
             position: fixed;
@@ -210,84 +213,85 @@
           }
         } */
         .loader {
-         position: relative;
-         width: 2.5em;
-         height: 2.5em;
-         transform: rotate(165deg);
+            position: relative;
+            width: 2.5em;
+            height: 2.5em;
+            transform: rotate(165deg);
         }
-        
-        .loader:before, .loader:after {
-         content: "";
-         position: absolute;
-         top: 50%;
-         left: 50%;
-         display: block;
-         width: 0.5em;
-         height: 0.5em;
-         border-radius: 0.25em;
-         transform: translate(-50%, -50%);
-        }
-        
-        .loader:before {
-         animation: before8 2s infinite;
-        }
-        
+
+        .loader:before,
         .loader:after {
-         animation: after6 2s infinite;
+            content: "";
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            display: block;
+            width: 0.5em;
+            height: 0.5em;
+            border-radius: 0.25em;
+            transform: translate(-50%, -50%);
         }
-        
+
+        .loader:before {
+            animation: before8 2s infinite;
+        }
+
+        .loader:after {
+            animation: after6 2s infinite;
+        }
+
         @keyframes before8 {
-         0% {
-          width: 0.5em;
-          box-shadow: 1em -0.5em rgba(225, 20, 98, 0.75), -1em 0.5em rgba(111, 202, 220, 0.75);
-         }
-     
-         35% {
-          width: 2.5em;
-          box-shadow: 0 -0.5em rgba(225, 20, 98, 0.75), 0 0.5em rgba(111, 202, 220, 0.75);
-         }
-     
-         70% {
-          width: 0.5em;
-          box-shadow: -1em -0.5em rgba(225, 20, 98, 0.75), 1em 0.5em rgba(111, 202, 220, 0.75);
-         }
-     
-         100% {
-          box-shadow: 1em -0.5em rgba(225, 20, 98, 0.75), -1em 0.5em rgba(111, 202, 220, 0.75);
-         }
+            0% {
+                width: 0.5em;
+                box-shadow: 1em -0.5em rgba(225, 20, 98, 0.75), -1em 0.5em rgba(111, 202, 220, 0.75);
+            }
+
+            35% {
+                width: 2.5em;
+                box-shadow: 0 -0.5em rgba(225, 20, 98, 0.75), 0 0.5em rgba(111, 202, 220, 0.75);
+            }
+
+            70% {
+                width: 0.5em;
+                box-shadow: -1em -0.5em rgba(225, 20, 98, 0.75), 1em 0.5em rgba(111, 202, 220, 0.75);
+            }
+
+            100% {
+                box-shadow: 1em -0.5em rgba(225, 20, 98, 0.75), -1em 0.5em rgba(111, 202, 220, 0.75);
+            }
         }
-        
+
         @keyframes after6 {
-         0% {
-          height: 0.5em;
-          box-shadow: 0.5em 1em rgba(61, 184, 143, 0.75), -0.5em -1em rgba(233, 169, 32, 0.75);
-         }
-     
-         35% {
-          height: 2.5em;
-          box-shadow: 0.5em 0 rgba(61, 184, 143, 0.75), -0.5em 0 rgba(233, 169, 32, 0.75);
-         }
-     
-         70% {
-          height: 0.5em;
-          box-shadow: 0.5em -1em rgba(61, 184, 143, 0.75), -0.5em 1em rgba(233, 169, 32, 0.75);
-         }
-     
-         100% {
-          box-shadow: 0.5em 1em rgba(61, 184, 143, 0.75), -0.5em -1em rgba(233, 169, 32, 0.75);
-         }
+            0% {
+                height: 0.5em;
+                box-shadow: 0.5em 1em rgba(61, 184, 143, 0.75), -0.5em -1em rgba(233, 169, 32, 0.75);
+            }
+
+            35% {
+                height: 2.5em;
+                box-shadow: 0.5em 0 rgba(61, 184, 143, 0.75), -0.5em 0 rgba(233, 169, 32, 0.75);
+            }
+
+            70% {
+                height: 0.5em;
+                box-shadow: 0.5em -1em rgba(61, 184, 143, 0.75), -0.5em 1em rgba(233, 169, 32, 0.75);
+            }
+
+            100% {
+                box-shadow: 0.5em 1em rgba(61, 184, 143, 0.75), -0.5em -1em rgba(233, 169, 32, 0.75);
+            }
         }
-        
+
         .loader {
-         position: absolute;
-         top: calc(50% - 1.25em);
-         left: calc(50% - 1.25em);
+            position: absolute;
+            top: calc(50% - 1.25em);
+            left: calc(50% - 1.25em);
         }
     </style>
 </head>
 
 <body>
-<div class="loader-container">
+    <div class="loader-container">
         <div class="loader"></div>
     </div>
     <div class="container">
