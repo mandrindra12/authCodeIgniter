a1c386970ae6786eb79fbaf85b649f688593498b
mandrindra12 <mandrindraantonnio@gmail.com>
2024-05-17 11:16:42 +0300

    php-csfixer action

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace App\Controllers;
+
 require 'vendor/autoload.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -21,49 +24,51 @@ class UserController extends BaseController
         return view('login');
     }
 
-	public function forgetPassword(){
-		return view('forgetPassword');
-	}
-	public function newPassword(){
-	return view('newPassword');
-	}
+    public function forgetPassword(): string
+    {
+        return view('forgetPassword');
+    }
+    public function newPassword(): string
+    {
+        return view('newPassword');
+    }
 
-	public function sendMail(){
-	$request= \Config\Services::request();
-	$destinataires=$request->getvar('mail');
-	$mail = new PHPMailer(true);
-	try{
-    // SMTP settings
-    $mail->isSMTP();
-    $mail->Host = 'mail.mit-ua.mg';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'fnomenjanahary@mit-ua.mg';
-    $mail->Password = 'G}Tc4jE%{c?+';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
+    public function sendMail(): \CodeIgniter\HTTP\RedirectResponse
+    {
+        $request = \Config\Services::request();
+        $destinataires = $request->getPost('mail');
+        $mail = new PHPMailer(true);
+        try {
+            // SMTP settings
+            $mail->isSMTP();
+            $mail->Host = 'mail.mit-ua.mg';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'fnomenjanahary@mit-ua.mg';
+            $mail->Password = 'G}Tc4jE%{c?+';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
 
-    // Sender and reply-to
-    $mail->setFrom('fnomenjanahary@mit-ua.mg', 'Nomenjanahary');
-    $mail->addReplyTo($destinataires, 'Fock');
+            // Sender and reply-to
+            $mail->setFrom('fnomenjanahary@mit-ua.mg', 'Nomenjanahary');
+            $mail->addReplyTo($destinataires, 'Fock');
 
-    // Add the recipient
-    $mail->addAddress($destinataires, 'Fock');
+            // Add the recipient
+            $mail->addAddress($destinataires, 'Fock');
 
-    // Email content
-    $mail->isHTML(true);
-    $mail->Subject = 'Mot de passe oublié';
-    $mail->Body    = 'Voici le lien pour renouveler votre mot de passe <a href="https://dev.authentication.mit/index.php/new-password/" >Click</a>';
-	//$mail->addAttachment($pdf);
+            // Email content
+            $mail->isHTML(true);
+            $mail->Subject = 'Mot de passe oublié';
+            $mail->Body    = 'Voici le lien pour renouveler votre mot de passe <a href="https://dev.authentication.mit/new-password/" >Click</a>';
+            //$mail->addAttachment($pdf);
 
-    $mail->send();	
-    echo "Message has been sent\n";
-	return redirect()->route('/');
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}\n";
-	return redirect()->to('/index.php/forget-password');
-}
-	
-	}
+            $mail->send();
+            echo "Message has been sent\n";
+            return redirect()->route('/');
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}\n";
+            return redirect()->to('/index.php/forget-password');
+        }
+    }
     public function inscriptionIndex(): string
     {
         return view('signup');
@@ -95,18 +100,18 @@ class UserController extends BaseController
             $user = $users[0];
             $this->usermodel->connexion($user['nom'], $user['prenoms']);
             $data = [
-              'id' => $user['id_personne'],
-              'nom' => $user["nom"],
-              'prenom' => $user["prenoms"],
-              'mot_de_passe' => $user["mot_de_passe"],
-              'statut' => $userStatus[0]['statut'],
-              'prefix' => $prefix
+                'id' => $user['id_personne'],
+                'nom' => $user["nom"],
+                'prenom' => $user["prenoms"],
+                'mot_de_passe' => $user["mot_de_passe"],
+                'statut' => $userStatus[0]['statut'],
+                'prefix' => $prefix
             ];
             $this->setSession($data);
             return $this->response
-              ->setContentType('application/json')
-              ->setStatusCode(200)
-              ->setJSON(['status' => 'log in successful']);
+                ->setContentType('application/json')
+                ->setStatusCode(200)
+                ->setJSON(['status' => 'log in successful']);
         }
     }
     public function connexion(): \CodeIgniter\HTTP\ResponseInterface
@@ -119,9 +124,9 @@ class UserController extends BaseController
         if (empty($userStatus)) {
             // echo json_encode(['status' => 'user not found', 'status_code' => 404]);
             return $this->response
-              ->setContentType('application/json')
-              ->setStatusCode(404)
-              ->setJSON(['status' => 'user not found']);
+                ->setContentType('application/json')
+                ->setStatusCode(404)
+                ->setJSON(['status' => 'user not found']);
         }
         // return $this->response->setJSON(['statut' => $users]);
         foreach ($users as $user) {
@@ -137,26 +142,26 @@ class UserController extends BaseController
                     // echo json_encode(['status' => 'loggin successful', 'status_code' => 200]);
                     $prefix = ($userStatus[0]['statut'] == 1) ? "Monsieur" : "Etudiant(e)";
                     $data = [
-                      'id' => $user['id_personne'],
-                      'nom' => $user["nom"],
-                      'prenom' => $user["prenoms"],
-                      'mot_de_passe' => $user["mot_de_passe"],
-                      'statut' => $userStatus[0]['statut'],
-                      'prefix' => $prefix
+                        'id' => $user['id_personne'],
+                        'nom' => $user["nom"],
+                        'prenom' => $user["prenoms"],
+                        'mot_de_passe' => $user["mot_de_passe"],
+                        'statut' => $userStatus[0]['statut'],
+                        'prefix' => $prefix
                     ];
                     // if($user <= 0) return redirect()->route('/');
                     $this->setSession($data);
                     return $this->response
-                      ->setContentType('application/json')
-                      ->setStatusCode(200)
-                      ->setJSON(['status' => 'log in successful']);
+                        ->setContentType('application/json')
+                        ->setStatusCode(200)
+                        ->setJSON(['status' => 'log in successful']);
                 }
             } else {
                 // echo json_encode(['status' => 'incorrect password', 'status_code' => 200]);
                 return $this->response
-                  ->setContentType('application/json')
-                  ->setStatusCode(403)
-                  ->setJSON(['status' => 'incorrect password']);
+                    ->setContentType('application/json')
+                    ->setStatusCode(403)
+                    ->setJSON(['status' => 'incorrect password']);
             }
         }
         return view('500.html');
@@ -167,20 +172,20 @@ class UserController extends BaseController
         if (!$this->personmodel->exist($userdata['nom'], $userdata['prenom'])) {
             // echo json_encode(['status' => 'no person found for this name in mit/misa', 'status_code'=> 404]);
             return $this->response->setContentType('application/json')->setStatusCode(404)
-              ->setJSON(['status' => 'no person found for this name in MIT/MISA']);
+                ->setJSON(['status' => 'no person found for this name in MIT/MISA']);
         } elseif ($this->usermodel->hasAccount($userdata['nom'], $userdata['prenom'])) {
             // echo json_encode(['status' => 'user has already an account, abort...', 'status_code' => 400]);
             return $this->response->setContentType('application/json')->setStatusCode(400)
-              ->setJSON(['status' => 'user has already an account, Abort...']);
+                ->setJSON(['status' => 'user has already an account, Abort...']);
         } else {
             $infoPerson = $this->personmodel->getRelativeInfo($userdata['nom'], $userdata['prenom']);
             $hashedpassword = password_hash($userdata['password'], PASSWORD_BCRYPT);
             $data = array(
-              'nom' => $userdata['nom'],
-              'prenoms' => $userdata['prenom'],
-              'mot_de_passe' => $hashedpassword,
-              'id_personne' => $infoPerson[0]['id_personne'],
-              'statut' => $infoPerson[0]['id_statut']
+                'nom' => $userdata['nom'],
+                'prenoms' => $userdata['prenom'],
+                'mot_de_passe' => $hashedpassword,
+                'id_personne' => $infoPerson[0]['id_personne'],
+                'statut' => $infoPerson[0]['id_statut']
             );
             $this->usermodel->inscription($data);
             return $this->response->setContentType('application/json')->setStatusCode(201)->setJSON(['status' => 'registration successful']);
