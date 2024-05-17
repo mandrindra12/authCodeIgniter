b0d8d21d5d615d13f376f08ec002d0c25497c482
mandrindra12 <mandrindraantonnio@gmail.com>
2024-05-17 09:31:40 +0300

    update models

--- app/Models/PersonModel.php
+++ app/Models/PersonModel.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class PersonModel extends Model {
+class PersonModel extends Model
+{
   protected $db;
   protected $builder;
   protected $table = 'personnes';
@@ -23,17 +24,19 @@ class PersonModel extends Model {
     'nationalite',
     'id_statut'
   ];
-  public function __construct() {
+  public function __construct()
+  {
     $this->db = db_connect();
-    $this->builder = $this->db->table($this->table); 
+    $this->builder = $this->db->table($this->table);
   }
-  public function exist($n, $p) {
+  public function exist($n, $p)
+  {
     $c = ['nom' => $n, 'prenoms' => $p];
     $count = $this->where($c)->countAllResults();
     return $count > 0;
   }
-  public function getRelativeInfo($nom, $prenoms) {
-    return $this->builder->select('id_personne, id_statut')->where(['nom'=> $nom, 'prenoms'=> $prenoms])->get()->getResultArray();
+  public function getRelativeInfo($nom, $prenoms): array
+  {
+    return $this->builder->select('id_personne, id_statut')->where(['nom' => $nom, 'prenoms' => $prenoms])->get()->getResultArray();
   }
 }
-
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,52 +1,60 @@
 <?php
 
 namespace App\Models;
+
 use CodeIgniter\Model;
 
-class UserModel extends Model {
+class UserModel extends Model
+{
   protected $table = 'utilisateurs';
   protected $builder;
   protected $db;
   protected $primaryKey = 'id_utilisateur';
   // public $db = \Config\Database::connect();
   protected $allowedFields = ['id_utilisateur', 'id_personne', 'nom', 'prenoms', 'mot_de_passe', 'est_actif', 'statut'];
-  public function __construct() {
+  public function __construct()
+  {
     // $builder = $db->table($this->table);
     $this->db = db_connect();
     $this->builder = $this->db->table($this->table);
   }
   // ATY NO INSERTION ANATY DATABASE
-  public function inscription($data) {
+  public function inscription($data)
+  {
     return $this->insert($data);
   }
-  // 
-  public function connexion(string $nom, string $prenoms): bool {
+  //
+  public function connexion(string $nom, string $prenoms): bool
+  {
     $conditions = ['nom' => $nom, 'prenoms' => $prenoms];
     $this->builder->set('est_actif', 1)->where($conditions)->update();
     return true;
   }
-  public function verifyPassword(string $nom, string $prenom, $mdp) {
+  public function verifyPassword(string $nom, string $prenom, $mdp): bool
+  {
     $conditions = ['nom' => $nom, 'prenoms' => $prenom];
     $users = $this->where($conditions)->get()->getResultArray();
     foreach ($users as $user) {
-      if(password_verify($mdp, $user['mot_de_passe'])) {
+      if (password_verify($mdp, $user['mot_de_passe'])) {
         return true;
       }
     }
     return false;
   }
-  public function hasAccount($nom, $prenom) {
+  public function hasAccount($nom, $prenom)
+  {
     $c = array('nom' => $nom, 'prenoms' => $prenom);
     $count = 0;
-    $count = $this->builder->select('statut')->where($c)->get()->getResultArray();
+    $count = $this->builder->select('id_personne, statut')->where($c)->get()->getResultArray();
     return $count;
   }
-  public function getInfo($nom , $prenom ) {  
+  public function getInfo($nom, $prenom): array
+  {
     $conditions = ['nom' => $nom, 'prenoms' => $prenom];
     return $this->where($conditions)->get()->getResultArray();
   }
-  public function deconnexion($id) {
+  public function deconnexion($id): void
+  {
     $this->builder->set('est_actif', 0)->where('id_personne', $id)->update();
   }
 }
-
